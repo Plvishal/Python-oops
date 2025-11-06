13b8d772d19cb975ce86c4e9cc4c5fc4f4c3be1a
vishaloreqo <vishal@oreqo.io>
2025-11-06 23:46:45 +0530

    first commit

---
+++ ENCAPSULATION.py
@@ -0,0 +1,39 @@
+"""
+Problem-1: Bank Account Balance
+Goal: Create a class BankAccount with private balance __balance.
+Add methods: deposit(), withdraw(), and get_balance().
+Hint: Use __balance to make it private.
+Focus: Private variables, getters & setters.
+"""
+
+"""
+Problem-2: Student Report Card
+Goal: Make a class Student that hides marks using a private variable.
+Allow adding marks via method, but not directly.
+Hint: Add add_marks() and show_result() methods.
+Focus: Data hiding & controlled access.
+"""
+
+""""
+Problem-3: Password Protectio
+Goal: Create a UserAccount class with a private password.
+Add methods set_password() and check_password().
+Hint: Don’t print password directly; use a verification method.
+Focus: Secure attribute management.
+"""
+
+""""
+Problem-4: Car Speed Control
+Goal: Create class Car with private attribute __speed.
+Methods: accelerate(), brake(), and show_speed().
+Hint: Prevent speed from going below 0.
+Focus: Encapsulated control logic.
+"""
+
+""""
+Problem-5: Employee Salary Update
+Goal: Class Employee with private _salary.
+Only an admin can change salary via update_salary() method.
+Hint: Check user role before changing salary.
+Focus: Validation & restricted modification.
+"""
